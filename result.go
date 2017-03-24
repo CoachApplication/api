78e3ba31edb299ad4c703673c3b15b255e1d60ba
james.nesbitt@wunder.io <james.nesbitt@wunder.io>
2017-03-24 19:16:16 +0200

    more result docs

--- result.go
+++ result.go
@@ -4,7 +4,17 @@ package api
 type Result interface {
 	// Return a slice of any errors that occured
 	Errors() []error
-	// Finished returns a tracking bool channel that can be used to mark when the operation is completed
+
+	/**
+	 * Finished returns a tracking bool channel that can be used to mark when the operation is completed
+	 *
+	 * The chan should be written to each time the result is marked finished (which typically only happens once).
+	 * This is only to make the Finished Marking safe to be repeated multiple times.
+	 * @TODO is the above behaviour appropriate?
+	 *
+	 * Note that if a Result is considered finished already when this chan is retrieved, it should still receive
+	 * an initial write, so that the chan can be read without any concern about chan read timeout.
+	 */
 	Finished() chan bool
 	// Success returns a boolean success value
 	Success() bool
